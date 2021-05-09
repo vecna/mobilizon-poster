67993a938c0448157a13bf0856f94e2ff33204cc
Claudio Agosti <claudio@tracking.exposed>
2021-05-09 18:57:24 +0200

    retested successfully bin/poster and bin/deleter

---
+++ bin/deleter.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+const fetch = require('node-fetch');
+const nconf = require('nconf');
+
+nconf.argv().env().file({file: "config.json"});
+
+async function deleter() {
+    const shared = require('../lib/shared');
+    shared.integrityChecks({'event':'required the numberic unique identified of as --event'});
+    return await fetch(nconf.get('api'), {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "authorization": "Bearer " + nconf.get('token'),
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        "pragma": "no-cache",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "sec-gpc": "1"
+      },
+      "referrerPolicy": "same-origin",
+      "body": "{\"operationName\":\"DeleteEvent\",\"variables\":{\"eventId\":\""+nconf.get('event')+"\"},\"query\":\"mutation DeleteEvent($eventId: ID!) {\\n  deleteEvent(eventId: $eventId) {\\n    id\\n    __typename\\n  }\\n}\\n\"}",
+      "method": "POST",
+      "mode": "cors"
+    }).then(async function(response) {
+        const d = await response.json();
+        console.log(d);
+        return d;
+    });
+}
+
+module.exports = {
+    deleter
+}
---
+++ bin/poster.js
@@ -0,0 +1,64 @@
+const _ = require('lodash');
+const moment = require('moment');
+const debug = require('debug')('mobilizon-poster');
+ 
+const event = require('../lib/event');
+const location = require('../lib/location');
+
+const nconf = require('nconf');
+nconf.argv().env().file({file: "config.json"});
+
+function fetchVariables(varmap) {
+    const retval = {};
+    const errors = _.compact(_.map(varmap, function(f, name) {
+        const readv = nconf.get(name);
+        debugger;
+        if(!readv || !readv.length)
+            return "--" + name + " is required";
+        
+        const lapz = f(readv);
+        if(!lapz)
+            return "--" + name + " invalid format? " + lapz;
+
+        console.log(name, lapz);
+        retval[name] = lapz;
+        return null;
+    }));
+    
+    if(errors.length) {
+        console.log(errors.join("\n"));
+        process.exit(1);
+    }
+    return retval;
+}
+
+async function poster() {
+    
+    const shared = require('../lib/shared');
+    shared.integrityChecks({
+        start: 'When the event begin: Format YYYY-MM-DD HH:mm',
+        end: 'When the event ends: Format YYYY-MM-DD HH:mm',
+        title: 'Event title',
+        description: 'Event description, you should supply the "\n" this way, if any',
+        url: 'Event URL, or Facebook URL, or leave it empty with ""',
+        address: "You should specify a full address — works: 'Street N, City, Region'"
+    });
+
+    const eventvars = fetchVariables({
+        start: moment,
+        end: moment,
+        title: _.toString,
+        description: _.toString,
+        url: _.toString,
+        address: _.toString,
+    })
+
+    eventvars.location = await location.queryLocation(eventvars.address);
+    debug(eventvars);
+   
+    await event.postToMobilizon(eventvars);
+    debug("Posted successfully, now listing the last four Event in the node:");
+    await event.fetchLastFourEvents();
+}
+
+poster();
