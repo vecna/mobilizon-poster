efb2f6dd6c955a2b972f712b932c5dbaf1333239
Simone Robutti <simone.robutti@protonmail.com>
2022-09-04 13:40:31 +0200

    added env from setupFiles

--- tests/integration/testLogin.js
+++ tests/integration/testLogin.js
@@ -6,11 +6,11 @@ nconf.argv().env();
 
 
 it('attempts a login against a mobilizon instance', async() => {
-console.log(nconf.get("username"))
+
 const username = nconf.get("username");
 const password = nconf.get("password");
 const api = nconf.get("api");
-
 const token = await login.perform({ login: username, password, api });
+
 const userInfo = await login.getInfo(token);
 console.log(token)
