99541a12d42a9db339c243656d2ae3897045b3e6
Simone Robutti <simone.robutti@protonmail.com>
2022-09-04 13:57:54 +0200

    added test condition

--- tests/integration/testLogin.js
+++ tests/integration/testLogin.js
@@ -10,7 +10,7 @@ const username = nconf.get("username");
 const password = nconf.get("password");
 const api = nconf.get("api");
 const token = await login.perform({ login: username, password, api });
-
 const userInfo = await login.getInfo(token);
-console.log(token)
+
+expect(token).toBeTruthy()
 });
