8d2d300c1ba306c81e8fcf5778744885889a600c
Simone <simone.robutti@protonmail.com>
2022-09-05 17:51:16 +0200

    improved docs

--- tests/integration/testLogin.js
+++ tests/integration/testLogin.js
@@ -4,11 +4,11 @@ const nconf = require('nconf');
 
 nconf.argv().env();
 
-it('attempts a login against a mobilizon instance and verifies the token', async() => {
+it('attempts a login against a mobilizon instance and verifies that the token is received', async() => {
 
 const email = nconf.get("email");
 const password = nconf.get("password");
 const api = nconf.get("api");
-const token = await login.perform({ email, password, api });
+const token = await login.perform( email, password, api);
 expect(token.length).toBeGreaterThan(20)
 });
